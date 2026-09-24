25ed454ccb90ab657e6739ebf86f06763d255290
suresh <vutavenkatasuresh@gmail.com>
2026-09-24 19:04:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0e919ce4a24bcae3b" # Replace with a valid AMI ID for your region
+  instance_type = "t3.micro"             # Free-tier eligible instance type
+
+  tags = {
+    Name = "MyFirstTerraformInstance"
+  }
+}
