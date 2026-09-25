02a4b8da7a2a80e02aa29f23ab64df0bab0447a6
suresh <vutavenkatasuresh@gmail.com>
2026-09-25 11:10:14 +0530

    Add AWS provider configuration to main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region = "ap-south-1" # Change to your preferred AWS region
+}
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0e919ce4a24bcae3b" # Replace with a valid AMI ID for your region
   instance_type = "t3.micro"             # Free-tier eligible instance type
